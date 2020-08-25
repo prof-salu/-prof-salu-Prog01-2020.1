1a358878caed73f829e0965a273a949867ae3518
prof-salu <saluorodrigues@gmail.com>
2020-08-24 21:50:09 -0300

    Arquivos aula03

---
+++ prog01-aula03/src/OlaMundoNovo.java
@@ -0,0 +1,21 @@
+public class OlaMundoNovo {
+	public static void main(String[] args) {
+		System.out.println("Ola mundo Java");
+		
+		//Comentario de linha
+		
+		/*
+		 * Comentario de bloco
+		 */
+	}
+}
+
+/*
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "Ola mundo Java";
+}
+*/
+
+//print("Ola mundo Java")
