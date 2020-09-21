37a222b61846335e90f7ba038316e1bcd9addba5
prof-salu <saluorodrigues@gmail.com>
2020-09-21 19:56:15 -0300

    arquivos aula06

--- Prog01-aula06/src/Conta.java
+++ Prog01-aula06/src/Conta.java
@@ -1,26 +1,34 @@
 
 public class Conta {
-	//Atributos
+	// Atributos
 	int numero;
 	int agencia;
 	double saldo;
 	String titular;
-	
-	//metodos
+
+	// metodos
 	boolean saca(double valor) {
 		double taxa = 0.5;
-		if(valor <= 0 || valor > saldo) {
+		if (valor <= 0 || valor > saldo) {
 			return false;
 		}
 		saldo = saldo - valor - taxa;
 		return true;
 	}
-	
+
 	boolean deposita(double valor) {
+		if(valor <= 0) {
+			return false;
+		}
+		saldo = saldo + valor;
 		return true;
 	}
-	
+
 	boolean transfere(double valor, Conta favorecido) {
-		return true;
+		if(saca(valor) == true) {
+			favorecido.deposita(valor);
+			return true;
+		}		
+		return false;
 	}
 }
--- Prog01-aula06/src/TestaConta.java
+++ Prog01-aula06/src/TestaConta.java
@@ -5,11 +5,17 @@ public class TestaConta {
 		
 		//Instanciando uma conta
 		Conta c1 = new Conta();
+		Conta c2 = new Conta();
 		
 		c1.agencia = 1234;
 		c1.numero = 5678;
 		c1.saldo = 7000;
 		c1.titular = "Pedro";
+		
+		c2.agencia = 1234;
+		c2.numero = 7788;
+		c2.saldo = 1000;
+		c2.titular = "Cintia";
 		
 		System.out.println("Saldo: " + c1.saldo);		
 		//Saque de 200
@@ -26,5 +32,20 @@ public class TestaConta {
 		
 		System.out.println("Saque efetuado? " + c1.saca(15000));
 		System.out.println("Saldo: " + c1.saldo);
+		
+		c1.deposita(800);
+		System.out.println("Saldo: " + c1.saldo);
+		
+		c1.deposita(-2000);
+		System.out.println("Saldo: " + c1.saldo);
+		
+		//Transferir 500 reais de C1 para C2
+		System.out.println("Antes da transferencia");
+		System.out.println("Saldo: " + c1.saldo);
+		System.out.println("Saldo: " + c2.saldo);
+		c1.transfere(500, c2);
+		System.out.println("Apos a transferencia");
+		System.out.println("Saldo: " + c1.saldo);
+		System.out.println("Saldo: " + c2.saldo);
 	}
 }
